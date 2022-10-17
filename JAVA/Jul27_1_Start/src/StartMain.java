f2dc9863fd76b236616d87688cecdd4d39197c67
SeoulPolarBear <96487511+SeoulPolarBear@users.noreply.github.com>
2022-10-17 16:33:30 +0900

    Java Start

---
+++ JAVA/Jul27_1_Start/src/StartMain.java
@@ -0,0 +1,117 @@
+
+/**
+ * @author user
+ *
+ */
+public class StartMain {
+	//주석(Comment) : 필기, 기능설명 용도
+	// \ : Slash
+	// \ : BackSlash
+	
+	// : 한줄 주석
+	/*여러줄 주석*/
+	
+	/**
+	 * 다른 사람들에게 보여주기 위한 설명서
+	 * @author Lim
+	 * @version 
+	 * @date 7 27
+	 * 
+	 * 
+	 *  : EUC-KR은 ㅂㅜㅔㄹㅂ ㅁㅜㅔㄱ 같은걸 compile을 못 한다.
+	 */
+		//Java!
+	//세심하게 하나하나 개발자가 손을 봐주야 한다.
+	//상대적으로 저급언어
+	//=> 먼저 Java를 공부한 후에 다른언어 사용시 습득시 좀더 쉽게 할 수 있다.
+	//개발자들이 작업한 자료들을 공개 / 공유하는 문화가 존재!
+	
+	
+	//openJDK => JRE + 실행 키트를 합쳐놓은 개념 
+	//JRE(JAVA Runtime Environment), JAVA로 만든 프로그램을 실행할 수 있게 하는 것
+	//Oracle JDK vs OpenJDK 
+	//Sun마이크로시스템즈에서 만든 제품 -Java
+	//Sun사가 Oracle에 인수를 당함
+	//Oracle의 제품 - Java
+	//	Oracle JDK(OpenJDK + 추가기능) : 유료
+	// OpenJDK : 무료
+	// 구버전 : 구하기가 Github가 훨씬 수월하다.
+	//그리고 이미 많은 회사들이 이미 OpenJDK를 사용하고 있다.
+	//LTS(Long Term Support)
+	
+	
+	
+	
+	
+	
+	/**javac.exe : JDK에 포함이 되어있는 주요한 Java의 컴파일러(Compiler)
+	 * 이 텍스트 파일(Hello)을 ByteCode(바이트 코드, 실행을 하기 위한 이진 표현법)로 변환, 
+	 * 이것을 Compile이라고 한다.
+	 * 이 컴파일 과정을 해주는게 javac.exe 파일이다.
+	 * 텍스트 파일을 javac.exe 로 실행하면, .class파일이 생성된 것을 볼 수 있다.
+	 * */
+	
+	/**java.exe
+	 * 컴파일된 .class 파일을 실행시켜주는 프로그램
+	 * 
+	 * 
+	 */
+	
+	/** 결론!
+	 * javac.exe : 영어, 한국어,... -번역 -> 기계어
+	 * java.exe : 기계어를 실행 시켜주는 역할을 한다.
+	 * 
+	 * */
+	
+	/**Eclipse
+	 * IntelliJ, VSCode, ...
+	 * 굳이 Eclipse를 선택한 이유 => 무료
+	 * .msi / .zip
+	 * .msi : 설치버전 : 한 컴퓨터에서 Eclipse를 여러개 사용 O
+	 * .zip : 압축버전 : 한 컴퓨터에서 Eclipse를 하나만 사용 O
+	 * 
+	 * 
+	 * Developer vs Enterprise
+	 * 기본적인 서비스만 제공 vs 기본 서비스 + 추가 기능
+	 * 
+	 * */
+	
+	
+	// Encoding
+	// 사람이 입력한 글자를 컴퓨터가 사용할 수 있게 신호로 바꿔주는 것
+	//MS949, UTF-8, EUC-KR, ...
+	
+	
+	
+	
+	/**단축키
+	*1. ctrl + s = 저장
+	*2. ctrl + a = 전체 선택
+	*3. ctrl + m = 화면 비율
+	*4. ctrl + shift + / = 여러 줄 주석처리
+	*5. ctrl + alt + 방향키(상하) = 그 줄을 기준으로(위/아래로 복사)
+	*6. alt + 방향키(상하) = 그 줄을 (위/아래와 alternate 혹은 이동하는 단축키)
+	*7. ctrl + shift + +/-(숫자키 옆) = 글자 크기를 키우거나 / 줄일 때 사용
+	*8. ctrl + d = 그 줄 삭제
+	*9. ctrl + space bar = 자동 완성 
+	*10. ctrl + shift + f = 줄정리 기능
+	*11. ctrl + shift + o = import 정리
+	*12. ctrl + f11 = 실행 기능
+	*13. ctrl + w = 창 종료
+	*14. ctrl + shift + numberpad(/) : 함수를 닫을 수 있다.
+	*15. ctrl + shift + numberpad(*) : 함수를 열 수 있다.
+	*16. ctrl + / : 함수 +,-표시
+	*17. alt + shift + r : 함수내 변수 중복 체크 및 수정
+	*18. ctrl + 원하는 변수, 객체, 함수,... 클릭 : 클릭한 변수, 객체, 함수,...의 원본이 있는 곳으로 이동
+	*19. ctrl + n : 뭔가 새롭게 만들 수 있는게 나온다.
+	*20. alt + shift + s : getter/setter생성
+	*
+	*/
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+	}
+}
