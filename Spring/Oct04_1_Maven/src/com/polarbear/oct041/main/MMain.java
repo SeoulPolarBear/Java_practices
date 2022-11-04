41407adc627cbe92eda866ce4775015e073c3e6a
SeoulPolarBear <96487511+SeoulPolarBear@users.noreply.github.com>
2022-11-04 18:06:53 +0900

    Maven

---
+++ Spring/Oct04_1_Maven/src/com/polarbear/oct041/main/MMain.java
@@ -0,0 +1,25 @@
+package com.polarbear.oct041.main;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import com.rabbit.aug231.main.LimHttpClient;
+
+//	Java 전 세계적으로 많이 사용
+//		개발자들이 본인이 만들어 놓은 것들을 공유!
+//		소스 자체를 공유 x, .jar파일 만들어서 공유하는 문화 O
+//		.jar파일을 자동으로 관리해주는 시스템 : Maven
+//		Maven 중앙 저장소 : https://mvnrepository.com/ (라이브러리들을 다 저장하고 있는 곳이다.) api가능
+
+//	Maven을 활용하면
+//		Maven 중앙 저장소에 저장되어 있는 .jar파일
+//		컴퓨터의 Maven로컬 저장소 : C:\Users\\user\.m2\repository
+
+//	Maven 중앙저장소에 없는 것들 : 로컬 저장소를 활용하면 됨!
+//		우리가 만든 보물들... dbm, hc,...
+
+public class MMain {
+	public static void main(String[] args) {
+		LimHttpClient phc;
+		XmlPullParser xpp;
+	}
+}
