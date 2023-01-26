b419c5e7cc8ec32f3479647c29a5fe61802383b0
SeoulPolarBear <96487511+SeoulPolarBear@users.noreply.github.com>
2023-01-27 03:02:48 +0900

    C OpenGL

---
+++ C/OpenGL/Projectexam1/Projectexam1/main.cpp
@@ -0,0 +1,34 @@
+
+#include<Windows.h>
+#include <gl/glut.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+
+
+	void MyDisplay() {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glColor3f(1.0, 0.0, 1.0);
+		glutWireTorus(1, 3, 15, 50);
+
+		glFlush();
+
+	}
+
+
+	int main(int argc, char** argv)
+	{
+		glutInit(&argc, argv);
+		glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+		glutCreateWindow("¿”º∫»Ø-201611587");
+		glutInitWindowPosition(80, 80);
+		glutInitWindowSize(1000, 1000);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+		glutDisplayFunc(MyDisplay);
+		glutMainLoop();
+
+		return 0;
+	}
